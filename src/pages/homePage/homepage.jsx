89f510259ef27808a4eda0c25660b6774d80d027
milindpawar007 <milindpawar5555@google.com>
2021-04-08 16:59:24 -0400

    testing

---
+++ src/pages/homePage/homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './homepage.styles.scss';
+import Directory from '../../component/directory/directory'
+import MenuItem from '../../component/menu-item/menu-item'
+
+const HomePage=()=>(
+  <div className='homepage'>
+
+             <Directory/>  
+
+  </div>
+
+)
+
+export default HomePage;
